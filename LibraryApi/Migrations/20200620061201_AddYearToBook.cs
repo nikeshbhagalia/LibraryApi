40b94764abc4d74a63d5ae2c940be95f2f5a5bad
Nikesh Bhagalia <nbha675@aucklanduni.ac.nz>
2020-06-20 18:13:44 +1200

    Added year to book

---
+++ LibraryApi/Migrations/20200620061201_AddYearToBook.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryApi.Migrations
+{
+    public partial class AddYearToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Books");
+        }
+    }
+}
